f805b9eb3ffd77cd7a2e3dc908cb2ae09c4f804b
Karine Valenca <valenca.karine@gmail.com>
2020-06-11 16:14:41 -0300

    created seed component

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'http://820c250f8435.ngrok.io'
+    baseURL: 'http://0f697292cf45.ngrok.io'
 })
 
 export default instance
--- src/screens/FarmScreen.js
+++ src/screens/FarmScreen.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import { View, Text } from 'react-native'
-import { Header } from 'react-native-elements'
+import { View, Text, StyleSheet } from 'react-native'
+import { Header, Card, Image } from 'react-native-elements'
+import SeedCard from '../components/SeedCard'
 
 const FarmScreen = () => {
     return(
@@ -24,10 +25,23 @@ const FarmScreen = () => {
                 
             />
         
-            <Text>Farm Screen</Text>
+            <Card containerStyle={{ borderRadius: 5 }} title="SEEDS">
+                <View style={styles.seedItemStyle}>
+                    <SeedCard image='image' name='Wheat' quantity='3'/>
+                    <SeedCard image='image' name='Rose' quantity='3'/>
+                    <SeedCard image='image' name='Rice' quantity='3'/>
+                </View>
+            </Card>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    seedItemStyle: {
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    }
+})
 
 FarmScreen.navigationOptions = () => {
     return {
