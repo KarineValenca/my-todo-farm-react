1ac76d3fb57f52f62083e6a6c414055d1e023b16
Karine Valenca <valenca.karine@gmail.com>
2020-06-14 14:31:56 -0300

    started plant screen

--- src/screens/CreatePlantScreen.js
+++ src/screens/CreatePlantScreen.js
@@ -1,8 +1,13 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, StyleSheet } from 'react-native'
+import { Card } from 'react-native-elements'
+import SeedCard from '../components/SeedCard'
 import CustomHeader from '../components/CustomHeader'
 
+import useSeeds from '../hooks/useSeeds'
+
 const CreatePlantScreen = ({ navigation }) => {
+    const [showUserSeeds, seeds] = useSeeds()
     return(
         <View>
             <CustomHeader 
@@ -11,10 +16,28 @@ const CreatePlantScreen = ({ navigation }) => {
                 leftOnClick={() => navigation.navigate('Farm')}
             />
             
-            <Text> Create Plant Screen </Text>
+            <Card containerStyle={{ borderRadius: 5 }} title="Which seed do you like to plant?" titleStyle={{ fontSize: 22 }}>
+                <View style={styles.cardItemStyle}>
+                    { seeds.map((l, i) => (  
+                        <SeedCard 
+                            key={i}
+                            image='image'
+                            name={l.name}
+                            quantity={l.quantity}
+                        />
+                    ))}
+                </View>
+            </Card>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    cardItemStyle: {
+        flexDirection: 'row',
+        justifyContent: 'space-around'
+    }
+})
 
 CreatePlantScreen.navigationOptions = () => {
     return {
